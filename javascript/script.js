93807cb9d33aba688efe08a73adeea5d2853f31b
nabeel <nabeel92914@outlook.com>
2024-03-11 15:33:05 +0000

    Completed Challenge 1 and added log as well

---
+++ javascript/script.js
@@ -0,0 +1,31 @@
+//Challange 1
+
+const isNumberPositive = (number) => {
+
+    if (number > 0) {
+        
+        console.log("True");
+
+    } else if (number < 0) {
+
+        console.log("False");
+
+    }
+}
+
+isNumberPositive(-1);
+isNumberPositive(10);
+
+//Challenge 2
+
+
+
+//Challenge 3
+
+
+
+//Challenge 4
+
+
+
+//Challenge 5
